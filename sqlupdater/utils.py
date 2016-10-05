5db18104438d70af9e6641cd63e7483861e078b1
ssola <introduccio@gmail.com>
2016-10-06 01:31:43 +0200

    fix issue getting config

--- sqlupdater/utils.py
+++ sqlupdater/utils.py
@@ -8,7 +8,10 @@ def get_config(config_path=None):
     Loads configuration from yaml file
     """
     if not config_path:
-        config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.yml')
+        config_path = os.path.join(
+            os.path.dirname(
+                os.path.dirname(os.path.abspath(__file__))
+            ), 'config.yml')
 
     if not os.path.exists(config_path):
         print colored("Error: ", "red") + " config path not found %s" % config_path
