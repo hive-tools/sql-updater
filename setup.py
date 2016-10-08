2523144ce2eb51bea1dc61efa3575bed6eb6628b
Sergio Sola <introduccio@gmail.com>
2016-10-08 13:35:28 +0000

    update setup

--- setup.py
+++ setup.py
@@ -10,5 +10,7 @@ setup(
     description="Up to date queries",
     author_email="introduccio@gmail.com",
     keywords=["HelloFresh", "BI", "Subscription Changes"],
-    packages=find_packages()
+    packages=find_packages(),
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
 )
