d06e11e218641249fa9e073afaa886f357b13254
Sergio Sola <introduccio@gmail.com>
2016-10-08 13:21:12 +0000

    created setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+NAME = "sqlupdater"
+VERSION = "0.0.1"
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Up to date queries",
+    author_email="introduccio@gmail.com",
+    keywords=["HelloFresh", "BI", "Subscription Changes"],
+    packages=find_packages()
+)
